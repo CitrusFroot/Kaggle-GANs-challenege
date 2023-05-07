5762c35d4cf6222f23135387b2f516b459bddebb
Jacob Reiss <jtreiss@wpi.edu>
2023-05-06 20:53:43 -0400

    data is loaded in, shuffled, and batched

--- driver.py
+++ driver.py
@@ -1,11 +1,46 @@
+import tensorflow as tf
+import numpy as np
+import csv
+
+
 def main():
-  pass
+  xy, apply = loadData()
+  visualizeData(xy)
+  train()
 
-def loadData():
-   pass
+#loads image data from tfrec files
+#useJPG: boolean, determines if we use jpgs instead of tfrec files
+#returns: 2 tf.data.Dataset objects, one holding the training data and the other holding images to be transformed
+def loadData(useJPG = 0):
+    if(useJPG == 0): #loads in the dataset from .tfrec files
+        trainingDataset = tf.data.TFRecordDataset('gan-getting-started\monet_tfrec').shuffle(buffer_size = 300).batch(BATCHSIZE)
+        trainingDataset = (trainingDataset.take(np.int64((1 - VALIDATIONSPLIT) * 300)),   #training split
+                           trainingDataset.skip(np.int64((1 - VALIDATIONSPLIT) * 300)))   #testing split
+        transformDataset = tf.data.TFRecordDataset('gan-getting-started\photo_tfrec').batch(BATCHSIZE)
+        return trainingDataset, transformDataset
+    
+    else: #we want to use jpgs
+        trainingDataset = tf.keras.utils.image_dataset_from_directory(directory= 'gan-getting-started\monet_jpg',
+                                                                      labels = 'inferred',
+                                                                      color_mode = 'rgb',
+                                                                      batch_size = BATCHSIZE,
+                                                                      image_size = IMAGESIZE,
+                                                                      shuffle = True,
+                                                                      seed = 1912,
+                                                                      validation_split= VALIDATIONSPLIT)
+        
+        transformDataset = tf.keras.utils.image_dataset_from_directory(directory= 'gan-getting-started\photo_jpg',
+                                                                      labels = 'inferred',
+                                                                      color_mode = 'rgb',
+                                                                      batch_size = BATCHSIZE,
+                                                                      image_size = IMAGESIZE,
+                                                                      shuffle = False,
+                                                                      validation_split= None)
+        return trainingDataset, transformDataset
 
-def visualizeData():
-   pass
+
+def visualizeData(dataset):
+   print(tf.)
 
 def generateArt():
    pass
@@ -15,4 +50,8 @@ def train():
 
 
 if __name__ == "__main__":
+    IMAGESIZE = 0
+    BATCHSIZE = 64
+    EPOCHS = 100
+    VALIDATIONSPLIT = 0.3
     main()
